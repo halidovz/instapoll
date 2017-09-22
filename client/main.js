c86afd3708ec16d5906317fddc15094457de02c6
Zelimkhan Khalidov <ondalom@gmail.com>
2017-09-22 18:50:29 +0300

    complete first version

--- client/main.js
+++ client/main.js
@@ -3,20 +3,239 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+var pollTypes = [{'name': 'стандартный опрос', type: 0}];
+
+Router.route('/', function () {
+  this.render('main');
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Router.route('/admin/:poll', {
+	waitOn: function() {
+    return Meteor.subscribe('poll', Session.get('currentPoll'));
   },
+  data: function() {
+    var pollId = this.params.poll;
+    Session.set('currentPoll', pollId);
+
+    var poll = Polls.findOne({privateId: pollId});
+    if(poll && poll.questions) {
+      _.forEach(poll.questions, function(q, index) {
+        q.index = index;
+        q.variants && _.forEach(q.variants, function(v, i) {
+          v.parent = q;
+          v.index = i;
+          if(q.right == v.id)
+            v.right = true;
+        })
+        q.participants && _.forEach(q.participants, function(p, i) {
+          p.parent = q;
+          p.index = i;
+        })
+      })
+    }
+    if(poll) {
+      poll.types = pollTypes.map(function(t) { return {type: t.type, name: t.name}});
+      if(!poll.type)
+        poll.type = 0;
+      _.forEach(poll.types, function(t, i) {
+        if(poll.type == t.type) {
+          t.selected = 1;
+          poll.typeName = t.name;
+        }
+      })
+      return poll;
+    }
+  },
+  template: 'admin'
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Router.route('/play', {
+  data: function() {
+
+    var poll = Polls.findOne({privateId: Session.get('currentPoll')});
+    
+    if(poll) {
+      if(!poll.type)
+        poll.type = 0;
+      poll.question = poll.questions[poll.currentQ];
+      if(poll.question.variants) {
+        poll.allCount = 0;
+
+        _.forEach(poll.question.variants, function(v) {
+          v.count = 0;
+          if(poll.question.answers) {
+            _.forEach(poll.question.answers, function(a) {
+              if(a == v.id) {
+                v.count++;
+                poll.allCount++;
+              }
+            })
+          }
+        })
+
+      }
+      poll.currentQ++;
+
+      var online = Presences.find();
+      poll.watchersCount = online.count();
+    }
+    return poll;
   },
+  template: 'play'
+});
+
+Router.route('/open/:poll', {
+	waitOn: function() {
+    return Meteor.subscribe('poll', Session.get('openedPoll'));
+  },
+  data: function() {
+    var pollId = this.params.poll;
+    Session.set('openedPoll', pollId);
+
+    var poll = Polls.findOne({openId: pollId});
+    if(poll && poll.questions) {
+      poll.question = poll.questions[0];
+      poll.question.variants && _.forEach(poll.question.variants, function(v, i) {
+        v.parent = poll.question;
+        if(poll.question.answers && v.id == poll.question.answers[poll.type == 1 ? Session.get('md5Participant') : Meteor.userId()] )
+          v.selected = true
+      })
+    }
+    return poll;
+  },
+  template: 'open'
+});
+
+Template.error.helpers({
+  errorMessage: function() {
+    return Session.get('error');
+  }
+})
+
+Router.configure({
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading',
+  onBeforeAction: function() {
+  	$('.modal').modal('hide');
+  	this.next();
+  }
+});
+
+var displayError = (function() {
+  var timeout;
+  return function(err) {
+    if(err) {
+      clearTimeout(timeout);
+      Session.set('error', err.message);
+      timeout = setTimeout(function() {
+        Session.set('error', '');
+      }, 3000);
+      return true;
+    }
+    return false;
+  }
+})()
+
+Template.main.events({
+	'submit .new-poll': function(e) {
+	  Meteor.call('createPoll', e.target.name.value, function(err, r) {
+	    if(displayError(err))
+	      return;
+	    Router.go('/admin/'+r);
+	    e.target.reset();
+	  });
+	  return false;
+	},
+	'submit .edit-poll': function(e) {
+	  Router.go('/admin/'+e.target.poll.value);
+	  return false;
+	},
+	'submit .open-poll': function(e) {
+	  Router.go('/open/'+e.target.poll.value);
+	  return false;
+	}
+});
+
+Template.admin.events({
+  'click .add-q': function(e) {
+    Meteor.call('addQuestion', Session.get('currentPoll'));
+    return false;
+  },
+  'click .add-p': function(e) {
+    Meteor.call('addParticipant', Session.get('currentPoll'));
+    return false;
+  },
+  'click .remove-q': function(e) {
+    Meteor.call('removeQuestion', Session.get('currentPoll'), {id: this.id});
+    return false;
+  },
+  'click .remove-p': function(e) {
+    Meteor.call('removeParticipant', Session.get('currentPoll'), {id: this.id});
+    return false;
+  },
+  'click .add-v': function(e) {
+    Meteor.call('addVariant', Session.get('currentPoll'), this.id);
+  },
+  'blur .poll-name': function(e) {
+    Meteor.call('changePoll', Session.get('currentPoll'), {name: e.target.value});
+  },
+  'blur .q-name': function(e) {
+    Meteor.call('changeQuestion', Session.get('currentPoll'), this.id, {name: e.target.value});
+  },
+  'click .poll-type': function(e) {
+    Meteor.call('changeType', Session.get('currentPoll'), this.type);
+  },
+  'blur .v-name': function(e) {
+    Meteor.call('changeVariant', Session.get('currentPoll'), this.id, this.index, {name: e.target.value});
+  },
+  'blur .p-name': function(e) {
+    Meteor.call('changeParticipant', Session.get('currentPoll'), this.id, {name: e.target.value});
+  },
+  'click .right-v': function(e, template) {
+    Meteor.call('rightVariant', Session.get('currentPoll'), this.parent.id, this.id);
+  },
+  'click .remove-v': function(e, template) {
+    Meteor.call('removeVariant', Session.get('currentPoll'), this.parent.id, {id: this.id});
+  },
+  'click .play': function() {
+    Meteor.call('switchQuestion', Session.get('currentPoll'), 0);
+    Router.go('/play');
+  },
+  'click .clear': function() {
+    Meteor.call('clear', Session.get('currentPoll'));
+  }
+});
+
+Template.play.events({
+  'click .switch-q': function(e) {
+    Meteor.call('switchQuestion', Session.get('currentPoll'), +e.target.dataset.direction);
+  }
+});
+
+Template.open.events({
+  'click .variant': function(e) {
+    Meteor.call('selectVariant', Session.get('openedPoll'), this.parent.id, this.id, Session.get('md5Participant'));
+  }
+})
+
+Tracker.autorun(function() {
+  var currentPoll, openedPoll;
+  if(currentPoll = Session.get('currentPoll')) {
+    Meteor.subscribe('poll', currentPoll);
+    var pollInfo = Polls.findOne({privateId: currentPoll});
+    if(pollInfo) {
+      Meteor.subscribe('pollWatchers', pollInfo.openId);
+    }
+  }
+  if(openedPoll = Session.get('openedPoll')) {
+    Meteor.subscribe('openPoll', openedPoll);
+  }
+});
+
+Template.registerHelper('percent', function(one, two) {
+  return (!two ? 0 : (one*100/two)).toFixed(2);
+});
+
+Template.registerHelper('equals', function (a, b) {
+	return a === b;
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  Accounts.removeOldGuests();
+
+  Meteor.publish("poll", function (poll) {
+    return Polls.find({privateId: poll});
+  });
+
+  Meteor.publish("pollWatchers", function (poll) {
+    var fields = {};
+    fields['state.poll'] = poll;
+    return Presences.find(fields);
+  });
+
+  Meteor.publishComposite('openPoll', function(poll) {
+    return {
+      find: function() {
+        return Polls.find({openId: poll}, {fields: {privateId: 0, questions: 0}});
+      },
+      children: [
+        {
+          find: function(pollInfo) {
+            return Polls.find({openId: poll}, {fields: {privateId: 0, 'questions.right': 0, questions: {$slice: [pollInfo.currentQ, 1]}}})
+          }
+        }
+      ]
+    }
+  });
 });
